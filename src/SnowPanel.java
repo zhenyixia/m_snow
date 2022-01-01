f1bca9467a579c2046fb6e5721d6626460438351
zhenyixia <zyx_2017@163.com>
2022-01-01 15:39:40 +0800

    修改雪花下落的宽与高

--- src/SnowPanel.java
+++ src/SnowPanel.java
@@ -2,12 +2,13 @@
  * @author Administrator
  * @since 2022/1/1 15:29
  **/
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class SnowPanel extends JPanel {
+public class SnowPanel extends JPanel{
   private int[] xx = new int[1000];
 
   private int[] yy = new int[1000];
@@ -18,18 +19,24 @@ public class SnowPanel extends JPanel {
 
   private int y = 100;
 
-  public SnowPanel() {
+  // 实际雪花 落下的高度
+  private int high = 1000;
+
+  // 实际雪花 落下的宽度
+  private int width = 1900;
+
+  public SnowPanel(){
     int i;
-    for (i = 0; i < this.xx.length; i++) {
-      this.xx[i] = (int)(Math.random() * 1500.0D);
-      this.yy[i] = (int)(Math.random() * 800.0D);
+    for(i = 0; i < this.xx.length; i++){
+      this.xx[i] = (int)(Math.random() * width);
+      this.yy[i] = (int)(Math.random() * high);
     }
     this.fs = new Font[12];
-    for (i = 0; i < this.fs.length; i++)
+    for(i = 0; i < this.fs.length; i++)
       this.fs[i] = new Font("宋体", 1, i + 12);
   }
 
-  public void paint(Graphics g) {
+  public void paint(Graphics g){
     super.paint(g);
     setBackground(Color.BLACK);
     g.setColor(Color.white);
@@ -37,27 +44,28 @@ public class SnowPanel extends JPanel {
     g.setColor(Color.black);
     g.fillArc(this.x, this.y, 70, 70, 0, 360);
     g.setColor(Color.white);
-    for (int i = 0; i < this.xx.length; i++) {
+    for(int i = 0; i < this.xx.length; i++){
       g.setFont(this.fs[i % 10]);
       g.drawString("*", this.xx[i], this.yy[i]);
     }
   }
 
-  public void startDown() {
-    (new Thread() {
-      public void run() {
-        while (true) {
+  public void startDown(){
+    (new Thread(){
+      public void run(){
+        while(true){
           SnowPanel.this.x = SnowPanel.this.x - 1;
           SnowPanel.this.y = SnowPanel.this.y - 1;
-          for (int i = 0; i < SnowPanel.this.yy.length; i++) {
+          for(int i = 0; i < SnowPanel.this.yy.length; i++){
             SnowPanel.this.yy[i] = SnowPanel.this.yy[i] + 1;
-            if (SnowPanel.this.yy[i] >= 800)
+            if(SnowPanel.this.yy[i] >= high){
               SnowPanel.this.yy[i] = 0;
+            }
           }
           SnowPanel.this.repaint();
-          try {
+          try{
             sleep(30L);
-          } catch (InterruptedException e) {
+          }catch(InterruptedException e){
             e.printStackTrace();
           }
         }
